ea42fe856cc364d877d595240cfc45fb1123489d
Skirly <475650215@qq.com>
2022-06-12 15:48:30 +0800

    第八版本（last）

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool mbPause=false;
+    public AudioMixer mixer;
+    [SerializeField] Button mPauseBtn;
+    public Slider mVolumeSlider;
     void Start()
     {
-        
+        mPauseBtn.onClick.AddListener(GamePause);
+        mVolumeSlider.onValueChanged.AddListener(GameVolumeChange);
     }
     public void GamePause()
     {
@@ -22,7 +28,10 @@ public class UIManager : MonoBehaviour
         }
         mbPause = !mbPause;
     }
-
+    public void GameVolumeChange(float fVolume)
+    {
+        mixer.SetFloat("MasterVolume",fVolume);
+    }
     // Update is called once per frame
     void Update()
     {
